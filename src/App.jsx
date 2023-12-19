7e2b81b5c490094858b6854582e81084bad69045
Bomowo <oeccc25@yahoo.com>
2023-12-19 06:49:37 +0200

    refactor code + add parser for snippet

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-
+import {useState} from 'react'
 import './App.css'
 
 function App() {
+  let [data, setData] = useState(null)
 
+  //regex for clean up html tags in snippet from wikimedia API
+  const regex = /(<([^>]+)>)/gi;
+
+  function getData () {
+    var url = "https://en.wikipedia.org/w/api.php?origin=*&action=query&list=search&format=json&srsearch=Nelson Mandela"; 
+
+    fetch(url)
+        .then(function(response){return response.json();})
+        .then(function(response) {
+          console.log(response)
+          setData({
+            title: response.query.search[0].title,
+            snippet: response.query.search[0].snippet
+          })
+        })
+    .catch(function(error){console.log(error);});
+  }
 
   return (
     <>
-      <h1>Hello World !</h1>
+      <h1 onClick={getData}>Hello World !</h1>
+      <h2>{data&&data.title}</h2>
+      <p>{data&&data.snippet.replace(regex, "")+'...'}</p>
     </>
   )
 }
